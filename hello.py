6c142728de985b224916327666f42774e4767990
Charlie Burris <cburris1983@gmail.com>
2019-04-23 06:24:43 -0400

    Add layouts.html to extend redudant boilerplate html markup.
    Added posts array for basic looping within jinja template.

--- hello.py
+++ hello.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Chaz B.',
+        'title': 'Blog Post #1',
+        'content' : 'Cool stuff in this post.',
+        'date_posted': 'April 23rd, 2019'
+    },
+    {
+        'author': 'Jane Do',
+        'title': 'Blog Post #2',
+        'content' : 'Cool stuff in 2nd post.',
+        'date_posted': 'April 24th, 2019'
+    }
+]
+
+
 @app.route('/')
 def base_route():
     return '<h1>Hello, from Flask</h1>'
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
