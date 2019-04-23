c1732cb5424bbc2f49507e0af0609ef109fba8eb
Charlie Burris <cburris1983@gmail.com>
2019-04-23 07:03:43 -0400

    Added .gitignore and format code to pep8.

--- hello.py
+++ hello.py
@@ -5,13 +5,13 @@ posts = [
     {
         'author': 'Chaz B.',
         'title': 'Blog Post #1',
-        'content' : 'Cool stuff in this post.',
+        'content': 'Cool stuff in this post.',
         'date_posted': 'April 23rd, 2019'
     },
     {
         'author': 'Jane Do',
         'title': 'Blog Post #2',
-        'content' : 'Cool stuff in 2nd post.',
+        'content': 'Cool stuff in 2nd post.',
         'date_posted': 'April 24th, 2019'
     }
 ]
@@ -21,13 +21,16 @@ posts = [
 def base_route():
     return '<h1>Hello, from Flask</h1>'
 
+
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
